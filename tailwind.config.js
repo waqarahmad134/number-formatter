5c67a2641aac3a9df4a07a6ed3f42e3bb09da4c6
Waqar Ahmad <waqar4704649@gmail.com>
2024-12-06 00:07:27 +0500

    bugs fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,12 +18,6 @@ module.exports = {
         lightBlack: "#1e1e1e",
         whiteSmoke: "#f8f6f3",
       },
-      backgroundImage: {
-        heroBg: "url('/rectangle-2@2x.png')",
-        exteriorHeroBg: "url('/images/hero.jpg')",
-        heroSectionBg: "url('/images/herobg.jpg')",
-        "payment-bg": "url('/images/payment-bg.webp')",
-      },
     },
   },
   plugins: [],
